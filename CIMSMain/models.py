241c2f79a7724eb8f33ef80389f93cb655a4c84c
warmb2009 <2462787827@qq.com>
2020-03-12 17:19:37 +0800

    对model进行修改

--- CIMSMain/models.py
+++ CIMSMain/models.py
@@ -8,31 +8,31 @@ class Meeting(models.Model):
     name = models.CharField(max_length=128, db_column='meeting_name')
 
     # 会议时间 django date
-    # date =
+    date = models.DateTimeField(db_column='meeting_time')
     
     # 会议地点 外键
-    # location =
-    
-    # 办会人员 一对多
-    # staffs =
+    location = models.ForeignKey('ConferenceRoom', db_column='conference_room', on_delete=models.CASCADE)
+
+    # 办会人员 多对多
+    staffs = models.ManyToManyField('Staff')
 
     # 办会单位 外键
-    # office =
+    office = models.ForeignKey('Office', db_column='office', on_delete=models.CASCADE)
 
     # 会议申请表(扫描件 图片) 外键
-    # form_page =
+    form_page = models.ForeignKey('FormPage', db_column='form_page', on_delete=models.CASCADE)
 
     # 办会级别 (部 省 市 县 科所) 外键
-    # from_level = 
+    from_level = models.ForeignKey('Level', db_column='from_level', on_delete=models.CASCADE)
     
     # 开至级别 (部 省 市 县 科所) 外键
-    # to_level
+    to_level = models.ForeignKey('Level', db_column='to_level', on_delete=models.CASCADE)
 
     # 创建条目日期 date
-    # add_date =
+    add_date = models.DateTimeField(db_column='add_date')
 
     # 修改日期 date
-    # modify_date
+    modify_date = models.DateTimeField(db_column='modify_date')
 
     
     def __str__(self):
@@ -61,8 +61,9 @@ class Set(models.Model):
 # 工作人员
 class Staff(models.Model):
     name = models.CharField(max_length=64)
-    phone = models.CharField(max_length=64) # 电话
-    office = ForeignKey()
+    phone1 = models.CharField(max_length=64) # 电话1
+    phone2 = models.CharField(max_length=64) # 电话2
+    office = ForeignKey('Office')
 
 # 工作身份 负责人 工作人员 两种    
 class Identity(models.Model):
