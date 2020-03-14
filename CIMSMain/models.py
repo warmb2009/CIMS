1948330fc74300869667aa0ea1301def77c537a2
warmb2009 <2462787827@qq.com>
2020-03-14 15:41:23 +0800

    修改模型

--- CIMSMain/models.py
+++ CIMSMain/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import django.utils.timezone as timezone
 
 '''
 
@@ -13,6 +14,12 @@ class Meeting(models.Model):
     # 会议地点 外键
     location = models.ForeignKey('ConferenceRoom', db_column='conference_room', on_delete=models.CASCADE, verbose_name='会议地点')
 
+    # 会议信道
+    channel = models.ForeignKey('Channel', db_column='channel', on_delete=models.CASCADE, verbose_name='省级会议信道', blank=True, null=True)
+
+    # 市局信道
+    local_channel = models.ForeignKey('LocalChannel', db_column='local_channel', on_delete=models.CASCADE, verbose_name='市级会议信道', blank=True, null=True)
+    
     # 办会人员 多对多
     staffs = models.ManyToManyField('Staff', verbose_name='办会人员')
 
@@ -20,7 +27,7 @@ class Meeting(models.Model):
     office = models.ForeignKey('Office', db_column='office', on_delete=models.CASCADE, verbose_name='办会单位')
 
     # 会议申请表(扫描件 图片) 外键
-    form_page = models.ForeignKey('FormPage', db_column='form_page', on_delete=models.CASCADE, verbose_name='会议申请表')
+    form_page = models.ForeignKey('FormPage', db_column='form_page', on_delete=models.CASCADE, verbose_name='会议申请表', null=True, blank=True)
 
     # 办会级别 (部 省 市 县 科所) 外键
     from_level = models.ForeignKey('Level', db_column='from_level', on_delete=models.CASCADE, related_name='fromlevel', verbose_name='级别')
@@ -29,11 +36,10 @@ class Meeting(models.Model):
     to_level = models.ForeignKey('Level', db_column='to_level', on_delete=models.CASCADE, related_name='tolevel', verbose_name='范围')
 
     # 创建条目日期 date
-    add_date = models.DateTimeField(db_column='add_date', verbose_name='创建条目日期')
+    add_date = models.DateTimeField(db_column='add_date', verbose_name='创建条目日期', auto_now_add=True)
 
     # 修改日期 date
-    modify_date = models.DateTimeField(db_column='modify_date', verbose_name='修改日期')
-
+    modify_date = models.DateTimeField(db_column='modify_date', verbose_name='最后修改日期', auto_now = True)
     
     def __str__(self):
         return self.name
@@ -79,10 +85,13 @@ class Set(models.Model):
 class Staff(models.Model):
     name = models.CharField(max_length=64, verbose_name='工作人员')
     phone1 = models.CharField(max_length=64, verbose_name='电话1') # 电话1
-    phone2 = models.CharField(max_length=64, verbose_name='电话2') # 电话2
+    phone2 = models.CharField(max_length=64, verbose_name='电话2', blank=True) # 电话2
 
     office = models.ForeignKey('Office', db_column='staff_office', on_delete=models.CASCADE, verbose_name='')
 
+    def __str__(self):
+        return self.name
+    
     class Meta:
         verbose_name_plural='工作人员'
 
@@ -91,6 +100,9 @@ class Staff(models.Model):
 class Identity(models.Model):
     name = models.CharField(max_length=64, verbose_name='工作身份')
 
+    def __str__(self):
+        return self.name
+    
     class Meta:
         verbose_name_plural='工作身份'
 
@@ -106,7 +118,31 @@ class FormPage(models.Model):
 # Level 等级 部 省 市 县 科所队
 class Level(models.Model):
     name = models.CharField(max_length=64, verbose_name='级别')
-
+    
+    def __str__(self):
+        return self.name
+    
     class Meta:
         verbose_name_plural='级别'
 
+
+# 省级信道
+class Channel(models.Model):
+    name = models.CharField(max_length=64, verbose_name='省级信道')
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural='省级信道'
+
+# 市级信道
+class LocalChannel(models.Model):
+    name = models.CharField(max_length=64, verbose_name='市级信道')
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural='市级信道'
+        
