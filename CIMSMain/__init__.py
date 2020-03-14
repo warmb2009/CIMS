8389e8816edd93622478c0e3e87b3a8a9e255975
warmb2009 <2462787827@qq.com>
2020-03-14 17:38:44 +0800

    添加中文描述

---
+++ CIMSMain/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'CIMSMain.apps.CimsmainConfig'
--- CIMSMain/apps.py
+++ CIMSMain/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class CimsmainConfig(AppConfig):
     name = 'CIMSMain'
+    verbose_name = '会议信息'
