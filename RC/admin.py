f3db811410947b4bfd5d2502d626bc06ae77e85c
warmb2009 <2462787827@qq.com>
2020-03-15 16:59:43 +0800

    加入RC

---
+++ RC/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(Daily)
+admin.site.register(RollCall)
+admin.site.register(OrderSet)
+admin.site.register(OrderReturn)
+admin.site.register(RollType)
+
---
+++ RC/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RcConfig(AppConfig):
+    name = 'RC'
+    verbose_name = '日常点名系统'
