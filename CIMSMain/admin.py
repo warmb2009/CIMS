8f9a8e09e1843631827b8c4e5cf01dfa4f10914c
warmb2009 <2462787827@qq.com>
2020-03-16 17:17:35 +0800

    主模型

--- CIMSMain/admin.py
+++ CIMSMain/admin.py
@@ -2,13 +2,13 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 admin.site.register(Meeting)
-admin.site.register(ConferenceRoom)
+#admin.site.register(ConferenceRoom)
 admin.site.register(Office)
 admin.site.register(Set)
 admin.site.register(Staff)
-admin.site.register(Identity)
-admin.site.register(FormPage)
-admin.site.register(Level)
-admin.site.register(Channel)
-admin.site.register(LocalChannel)
+#admin.site.register(Identity)
+#admin.site.register(FormPage)
+#admin.site.register(Level)
+#admin.site.register(Channel)
+#admin.site.register(LocalChannel)
 
