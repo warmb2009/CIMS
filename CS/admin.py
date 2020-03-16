bc2d90b167980f336ef8ce2e54beccb9849a51f1
warmb2009 <2462787827@qq.com>
2020-03-16 17:18:05 +0800

    修改通信录模型

--- CS/admin.py
+++ CS/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 admin.site.register(Staff)
-admin.site.register(Set)
+#admin.site.register(Set)
 admin.site.register(Title)
