b0bd2fd015520ba5916eab8a8b9a08664bae5380
warmb2009 <2462787827@qq.com>
2020-03-24 16:15:55 +0800

    加入测试api路由

--- CIMS/urls.py
+++ CIMS/urls.py
@@ -27,5 +27,6 @@ urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
+    url(r'^api/$', views.MettingsAPIVIew.as_view(), name='meetings'),
 ]
 
