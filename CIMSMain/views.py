679a036aa265bcf7bf8afa9a0a9f786ce6fb9da5
warmb2009 <2462787827@qq.com>
2020-03-28 18:30:23 +0800

    添加模板视图

--- CIMSMain/views.py
+++ CIMSMain/views.py
@@ -3,8 +3,13 @@ from .models import Meeting
 from rest_framework import viewsets
 from .serializers import MeetingSerializer
 from django.views.generic.base import View
-from django.http import HttpResponse,JsonResponse
+from django.http import HttpResponse, JsonResponse
+from django.core import serializers
 
+
+def index(request):
+    return render(request, 'index.html')
+    
 
 class MeetingViewSet(viewsets.ModelViewSet):
     """
@@ -13,42 +18,65 @@ class MeetingViewSet(viewsets.ModelViewSet):
     queryset = Meeting.objects.all().order_by('-date')
     serializer_class = MeetingSerializer
 
+
 class MeetingsAPIView(View):
-   """
-   查询所有会议
-   """
-   def get(self, request):
-       """
-       查询所有图书
-       路由：GET /books/
-       """
-       queryset = Meeting.objects.all()
-       meeting_list = []
-       for meeting in queryset:
-           meeting_list.append({
+    """
+    查询所有会议
+    """
+
+    def get(self, request):
+        """
+        查询所有图书
+        路由：GET /books/
+        """
+        queryset = Meeting.objects.all()
+        meeting_list = []
+        for meeting in queryset:
+            meeting_list.append({
                'id': meeting.id,
                'mtitle': meeting.name,
-               'mdate': meeting.date,               
-           })
-       return JsonResponse(meeting_list, safe=False)
+               'mdate': meeting.date,
+            })
+        return JsonResponse(meeting_list, safe=False)
 
 
 class MeetingAPIView(View):
-   def get(self, request, pk):
-       """
-       获取单个会议信息
-       路由： GET  /meetings/<pk>/
-       """
-       try:
-           meeting = Meeting.objects.get(pk=pk)
-       except Meeting.DoesNotExist:
-           return HttpResponse(status=404)
+    def get(self, request, pk):
+        """
+        获取单个会议信息
+        路由： GET  /meetings/<pk>/
+        """
+        try:
+            meeting = Meeting.objects.get(pk=pk)
 
-       return JsonResponse({
-           'id': meeting.id,
-           'btitle': meeting.name,
-           #'bpub_date': meeting.bpub_date,
-           #'bread': meeting.bread,
-           #'bcomment': meeting.bcomment,
-           #'image': meeting.image.url if book.image else ''
-       })
+            channel_all = meeting.channel.all()
+            localchannel_all = meeting.local_channel.all()
+           
+            channel_list = []
+            localchannel_list = []
+
+            for channel in channel_all:
+                channel_list.append(channel.name)
+            
+            for localchannel in localchannel_all:
+                localchannel_list.append(localchannel.name)
+            
+        except Meeting.DoesNotExist:
+            return HttpResponse(status=404)
+
+        return JsonResponse({
+            'id': meeting.id,
+            'mtitle': meeting.name,
+            'mdate': meeting.date,
+            'mschannel' : channel_list,
+            'mlchannel' : localchannel_list,
+            'mstaffs':meeting.staffs.name,
+            'moffice':meeting.office.name,
+            'mfromlevel':meeting.from_level.name,
+            'mtolevel':meeting.to_level.name,
+
+            # 'bpub_date': meeting.bpub_date,
+            # 'bread': meeting.bread,
+            # 'bcomment': meeting.bcomment,
+            # 'image': meeting.image.url if book.image else ''
+        })
